105059985fe6599641ad13f3fc738928cd75f4e0
maxg100707 <maxguzman914@gmail.com>
2025-01-06 09:49:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -196,3 +196,24 @@ uploadTask.on(
     notaForm.reset();
   }
 );
+/*
+        _             _
+       / \           / \
+      /   \         /   \
+     /     \_______/     \
+    /  ___           ___  \
+   |  / ()\         / ()\  |
+   |  \___/         \___/  |
+   |           ___         |
+   \          |___|        /
+    \           |         /
+     \      \___|___/    /
+      \_               _/
+        \_____________/  
+           MIAW MIAW
+
+
+instagram : https://www.instagram.com/maxg1007/
+portifolio : https://maxg100707.github.io/Portifolio/
+
+*/
